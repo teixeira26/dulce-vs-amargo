85d796416b4926bf1dd5e98fe772e86a3ca101d0
shift <teixeira.matheus2626@gmail.com>
2022-10-26 11:35:33 -0300

    (*) - desafio completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { ImgHTMLAttributes, RefObject, useRef } from 'react';
+import './app.css'
 function App() {
+  const container1 = useRef<HTMLDivElement>(null)
+  const container2 = useRef<HTMLDivElement>(null)
+  const img1 = useRef<HTMLImageElement>(null);
+  const img2 = useRef<HTMLImageElement>(null);
+
+  const cambiarWidth = (e:React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+    console.log(e.target)
+    
+    if(img1.current === e.target){
+      container1.current && container1.current.classList.add('crecer');
+      container2.current&& container2.current.classList.add('disminuir')
+    }
+    else{
+      container2.current && container2.current.classList.add('crecer')
+      container1.current && container1.current.classList.add('disminuir')
+    }
+  }
+  const volverAlNormal = (e:React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+    container1.current && container1.current.classList.remove('crecer');
+    container1.current && container1.current.classList.remove('disminuir');
+    container2.current && container2.current.classList.remove('crecer')
+    container2.current && container2.current.classList.remove('disminuir')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container1'  ref={container1} onMouseEnter={(e)=>cambiarWidth(e)} onMouseLeave={(e)=>volverAlNormal(e)}>
+        <img src="./assets/mateAmargo.jpeg"  ></img>
+        <div className='fondo1' ref={img1}>
+          <h1>Mate Amargo 😍​</h1>
+          <button>Votar</button>
+          </div>
+      </div>
+
+      <div className='container2' ref={container2}  onMouseEnter={(e)=>cambiarWidth(e)} onMouseLeave={(e)=>volverAlNormal(e)}>
+        <img src="./assets/mateDulce.jpeg" ></img>
+        <div className='fondo2'ref={img2}>
+          <h1>Mate Dulce 🥰​​</h1>
+          <button>Votar</button>
+        </div>
+      </div>
+     
     </div>
   );
 }
